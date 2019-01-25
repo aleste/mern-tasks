44f293bc78f5b4744b0d804578ac6d4609538e71
Alejandro Steinmetz <asteinmetz78@gmail.com>
2019-01-25 13:18:20 -0300

    Create models and backend api

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 
+const { mongoose } = require('./database');
+
 const app = express();
 
 // Settings
@@ -13,7 +15,6 @@ app.use(express.json());
 
 // Routes
 app.use('/api/tasks', require('./routes/task.routes'));
-
 
 // Statics files
 app.use(express.static(path.join(__dirname, 'public')))
